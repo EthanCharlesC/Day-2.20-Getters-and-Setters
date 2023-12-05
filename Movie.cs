df7c6469bd0ab54656eb978467c8c60fa88c7099
Ethan Chinnasamy <ethan.chinnasamy@standardbank.co.za>
2023-12-05 13:39:45 +0200

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2._20_Getters_and_Setters
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        public string rating;
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            rating = aRating;
+
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+
+            set
+            {
+                if (value =="G" || value == "PG" ||
+                    value == "PG-13" || value == "R")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2._20_Getters_and_Setters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Titanic", "Jim", "PG-13");
+            Movie movie2 = new Movie("Unreleased", "Evens", "PG");
+
+            Console.WriteLine(movie1.rating);
+            Console.ReadKey();
+        }
+    }
+}
